6e412795bbedc553d00cde68a8db0006d3329d5a
“Saya” <61030238@kmitl.ac.th>
2019-11-09 21:23:24 +0700

    7

--- 61030238/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 61030238/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -25,11 +25,12 @@ namespace WindowsFormsApp1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Brush mybrush = new SolidBrush(Color.DarkGreen);
+            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical,Color.White, Color.Violet);
             Pen mypen = new Pen(mybrush, 5);
             g.DrawEllipse(mypen, 10, 10, 200, 200);
             mypen.Dispose();
             mybrush.Dispose();
+
 
 
 
