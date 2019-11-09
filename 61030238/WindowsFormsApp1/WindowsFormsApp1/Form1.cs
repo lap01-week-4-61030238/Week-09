d61d76125b925da177d1348ef007b8c3648aaf65
“Saya” <61030238@kmitl.ac.th>
2019-11-09 21:06:10 +0700

    2

--- 61030238/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ 61030238/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -27,7 +27,12 @@ namespace WindowsFormsApp1
             Graphics g = e.Graphics;
             Pen bluepen = new Pen(Color.Blue, 2);
             g.DrawRectangle(bluepen, 10, 10, 100, 100);
-            bluepen.Dispose(); 
+            bluepen.Dispose();
+
+            Pen somepen = new Pen(Color.FromArgb(255, 120, 200));
+            g.DrawEllipse(somepen, 20, 20, 200, 200);
+            somepen.Dispose();
+
 
         }
     }
